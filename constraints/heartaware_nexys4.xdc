7913327434dbc28788d3fed55bf609bcca051a92
Michael Holachek <holachek@mit.edu>
2015-11-28 20:12:55 -0500

    Updated clock dividers.

--- constraints/heartaware_nexys4.xdc
+++ constraints/heartaware_nexys4.xdc
@@ -14,7 +14,7 @@
 ## Clock signal
 set_property -dict {PACKAGE_PIN E3 IOSTANDARD LVCMOS33} [get_ports CLK100MHZ]
 create_clock -period 10.000 -name sys_clk_pin -waveform {0.000 5.000} -add [get_ports CLK100MHZ]
-set_property SEVERITY {Warning} [get_drc_checks LUTLP-1];
+## set_property SEVERITY {Warning} [get_drc_checks LUTLP-1];
 ## set_property SEVERITY {Warning} [get_drc_checks NSTD-1];
 ## set_property SEVERITY {Warning} [get_drc_checks UCIO-1];
 
--- verilog/main.v
+++ verilog/main.v
@@ -77,33 +77,29 @@ module heartaware(
 //////////////////////////////////////////////////////////////////////////////////
 // create system and peripheral clocks, synced switches, master system reset
 
-    wire master_reset;
-    wire master_test;
+  wire master_reset;
+  wire master_test;
 
-    wire clk_100mhz = CLK100MHZ; // master clock
-    wire clk_65mhz; // VGA clock
-    wire clk_25mhz; // SD clock
-    wire clk_48khz; // audio sample rate clock
-    wire clk_1hz;
-    
-    wire clk_tone;
-    reg [31:0] tone_divider = 32'd250_000;
-    
-    clk_wiz_0 clk_65mhz_inst(.clk_100mhz(clk_100mhz), .clk_65mhz(clk_65mhz), .reset(master_reset));
-    clock_divider clk_25mhz_inst(.clk_in(clk_100mhz), .clk_out(clk_25mhz), .divider(32'd4), .reset(master_reset)); // 100_000_000 / 25_000_000 = 4
-    clock_divider clk_48khz_inst(.clk_in(clk_100mhz), .clk_out(clk_48khz), .divider(32'd2083), .reset(master_reset)); // 100_000_000 / 48_000 = 2083.33
-    clock_divider clk_tone_inst(.clk_in(clk_100mhz), .clk_out(clk_tone), .divider(tone_divider), .reset(master_reset));
-    clock_divider clk_1hz_inst(.clk_in(clk_100mhz), .clk_out(clk_1hz), .divider(32'd100_000_000), .reset(master_reset));
+  wire clk_100mhz = CLK100MHZ; // master clock, connected to hardware crystal oscillator
+  wire clk_65mhz; // VGA clock
+  wire clk_25mhz; // SD clock
+  wire clk_48khz; // audio sample rate clock
+  wire clk_1hz;
+      
+  clk_wiz_0 clk_65mhz_module(.clk_100mhz(clk_100mhz), .clk_65mhz(clk_65mhz), .reset(master_reset));
+  clock_divider clk_25mhz_module(.clk_in(clk_100mhz), .clk_out(clk_25mhz), .divider(32'd2), .reset(master_reset)); // 100_000_000 / 25_000_000 = 4
+  clock_divider clk_48khz_module(.clk_in(clk_100mhz), .clk_out(clk_48khz), .divider(32'd1042), .reset(master_reset)); // 100_000_000 / 48_000 = 2083.33
+  clock_divider clk_1hz_module(.clk_in(clk_100mhz), .clk_out(clk_1hz), .divider(32'd200_000_000), .reset(master_reset));
 
-    wire [15:0] sw_synced;
-    genvar i;
-    generate   for(i=0; i<16; i=i+1) 
-      begin: gen_modules  // generate 16 synchronize modules
-        synchronize s(clk_25mhz, SW[i], sw_synced[i]);
-      end
-    endgenerate
+  wire [15:0] sw_synced;
+  genvar i;
+  generate   for(i=0; i<16; i=i+1) 
+    begin: gen_modules  // generate 16 synchronize modules
+      synchronize s(clk_25mhz, SW[i], sw_synced[i]);
+    end
+  endgenerate
 
-    assign master_reset = SW[15];
+  assign master_reset = SW[15];
     
 
 
@@ -132,18 +128,14 @@ module heartaware(
 // 7 segment display related utilities
 
   reg [31:0] display_data;
+  wire [6:0] display_segments;
   
-
-
-  // assign data = {'hFFFF, 'b0, SW[14:0]};
-
-  wire [6:0] segments;
-  display_8hex display(.clk(clk_100mhz), .data(display_data), .seg(segments), .strobe(AN));     // digit strobe
-  assign SEG[6:0] = segments;
+  display_8hex display(.clk(clk_100mhz), .data(display_data), .seg(display_segments), .strobe(AN));
+  
+  assign SEG[6:0] = display_segments;
   assign SEG[7] = 1'b1;   // decimal point off
 
-
-
+// assign data = {...}
 
 
 
@@ -189,117 +181,152 @@ module heartaware(
 //////////////////////////////////////////////////////////////////////////////////
 // create all objects related to PWM audio output
 
-  wire [7:0] audio_data;
-  reg [7:0] audio_data_modified;
+  wire [7:0] pwm_audio_sample_data;
   wire pwm_en = 1;
   
-  assign AUD_SD = pwm_en;
-
   audio_PWM audio_PWM_module(.clk(clk_100mhz), .reset(master_reset),
-        .music_data(audio_data_modified), .PWM_out(AUD_PWM));
+        .music_data(pwm_audio_sample_data), .PWM_out(AUD_PWM));
 
+  assign AUD_SD = pwm_en;
 
 
 // SD CARD
 //////////////////////////////////////////////////////////////////////////////////
 // SD card objects
 
-//  wire spiMiso = 1;
-//  reg rd; // when ready is high, asserting rd will begin a read
-//  wire wr = 0;
-//  wire rst = master_reset;
-//  reg [31:0] adr; // address of read operation
-//  wire [7:0] sd_din;
-//  wire [7:0] sd_dout; // data output for read operation
-//  wire byte_available; // signal that a new byte has been presented on dout
-//  wire ready_for_next_byte;
-//  wire [4:0] state; // for debug purposes
+  // general SD signals
+  reg sd_rd; // when ready is high, asserting rd will begin a read
+  wire sd_wr = 0;
+  wire sd_ready;
+  wire [4:0] sd_state; // for debug purposes
+  
+  // read SD signals
+  reg [31:0] sd_adr; // address of read operation
+  wire [7:0] sd_dout; // data output for read operation
+  wire sd_byte_available; // signal that a new byte has been presented on dout
+  
+  // write SD signals
+  wire [7:0] sd_din;
+  wire sd_ready_for_next_byte;
+  
 
-//  sd_controller sd_controller_module(.cs(SD_DAT[3]), .mosi(SD_CMD), .miso(spiMiso),
-//        .sclk(SD_SCK), .rd(rd), .wr(wr), .reset(rst),
-//        .din(sd_din), .dout(sd_dout), .byte_available(byte_available),
-//        .ready(ready), .address(adr), 
-//        .ready_for_next_byte(ready_for_next_byte), .clk(clk_25mhz), 
-//        .status(state));
+  sd_controller sd_controller_module(.cs(SD_DAT[3]), .mosi(SD_CMD), .miso(SD_DAT[0]),
+        .sclk(SD_SCK), .rd(sd_rd), .wr(sd_wr), .reset(master_reset),
+        .din(sd_din), .dout(sd_dout), .byte_available(sd_byte_available),
+        .ready(sd_ready), .address(sd_adr), .clk(clk_25mhz), 
+        .ready_for_next_byte(sd_ready_for_next_byte), .status(sd_state));
 
-  reg [7:0] sd_dout;
-  reg wr_en;
-  reg rd_en;
+
+
+  reg fifo_wr_en;
+  reg fifo_rd_en;
+  
+  wire [7:0] fifo_dout;
+  assign fifo_dout = pwm_audio_sample_data;
 
   wire fifo_full;
   wire fifo_empty;
+  wire fifo_almost_empty;
   wire [10:0] fifo_count;
-  reg [7:0] counter;
 
-  fifo_generator_0 audio_sample_buffer(.clk(clk_100mhz), .rst(master_reset), .din(sd_dout), .wr_en(wr_en),
-        .rd_en(rd_en), .dout(audio_data), .full(fifo_full), .empty(fifo_empty), .data_count(fifo_count));
+  fifo_generator_0 audio_sample_buffer(.clk(clk_100mhz), .rst(master_reset), .din(sd_dout), .wr_en(fifo_wr_en),
+        .rd_en(fifo_rd_en), .dout(fifo_dout), .full(fifo_full), .empty(fifo_empty),
+        .data_count(fifo_count), .almost_empty(fifo_almost_empty));
 
-    reg last_btn_left = 0;
-    reg last_btn_right = 0;
-    
-  
-  always @ (posedge clk_tone) begin
-  
-      if (audio_data_modified)
-         audio_data_modified <= 0;
-      else
-         audio_data_modified <= audio_data;
-  
-  end
-  
 
+
+
+
+  reg last_clk_48khz;
+  reg last_btn_up;
+  reg last_btn_down;
+  reg last_btn_center;
+  reg last_btn_left;
+  reg last_btn_right;
+  reg [3:0] read_counter = 0;
+  reg read_new_sample;
 
   always @ (posedge clk_100mhz) begin
   
-    if (master_reset) begin
-        counter <= 0;
-    end
-  
-    // display_data[7:0] <= audio_data[7:0];  
-    // display_data[15:8] <= counter;
-    display_data[31:0] <= tone_divider;
-    LED[10:0] <= fifo_count[10:0];
-  
-    LED16_R <= fifo_full;
-    LED16_B <= fifo_empty;
-  
+    last_clk_48khz <= clk_48khz;
+    last_btn_up <= btn_up;
+    last_btn_down <= btn_down;
+    last_btn_center <= btn_center;
     last_btn_left <= btn_left;
     last_btn_right <= btn_right;
-
+    
+    fifo_wr_en <= sd_byte_available;
   
-    // WRITE TO FIFO
-    if (btn_left) begin
-        if (last_btn_left == 0) begin
-            tone_divider <= tone_divider - 5000;
-            LED17_R <= 1;
-            counter <= counter + 1;
-            if (SW[7:0] == 0) begin
-                sd_dout <= counter[7:0];
-            end else begin
-                sd_dout <= SW[7:0];
-            end
-            wr_en <= 1;
-        end else begin
-            wr_en <= 0;
-            LED17_R <= 0;
-        end
+    if (btn_up == 1 && last_btn_up == 0) begin
+        sd_adr <= sd_adr + 32'd512;
     end
-        
-    // READ FROM FIFO
-    if (btn_right) begin
-        if (last_btn_right == 0) begin
-            tone_divider <= tone_divider + 5000;
-            LED17_G <= 1;
-            rd_en <= 1;
-        end else begin
-            rd_en <= 0;
-            LED17_G <= 0;
-        end
+  
+    if (btn_center == 1 && last_btn_center == 0) begin
+        read_new_sample <= 1;
     end
-                    
+    
+    if (read_new_sample == 1 && sd_ready == 1) begin // && read_counter < 9
+        // read_counter <= read_counter + 1;
+        sd_rd <= 1;
+        fifo_wr_en <= 1;
+    end else if (read_counter == 9) begin
+        read_counter <= 0;
+        sd_rd <= 0; // comment out perhaps
+        read_new_sample <= 0;
+        fifo_wr_en <= 0;
+    end
+    
+     // FIFO user interface
+//    if (btn_left == 1 && last_btn_left == 0) begin
+//        fifo_wr_en <= 1;
+//    end else if (btn_left == 1 && last_btn_left == 1) begin
+//        fifo_wr_en <= 0;
+//    end
+    
+//    if (btn_right == 1 && last_btn_right == 0) begin
+//       fifo_rd_en <= 1;
+//    end else if (btn_right == 1 && last_btn_right == 1) begin
+//        fifo_rd_en <= 0;
+//    end
   
+//    if (clk_48khz == 1 && last_clk_48khz == 0) begin
+//        fifo_rd_en <= 1; // will output a new sample on fifo_dout
+//    end else 
+//        fifo_rd_en <= 0;
+//    end
+    
+//    if (fifo_almost_empty == 1 && sd_ready == 1 && sd_rd == 0) begin
+//        // load more samples from the SD card into FIFO buffer
+//        sd_adr <= sd_adr + 32'd512; // increment read address by 512 bytes
+//        sd_rd <= 1;
+//    end else begin
+//        sd_rd <= 0;
+//    end
   
+  if (master_reset) begin
+      read_counter <= 0;
+      sd_rd <= 0;
+      fifo_rd_en <= 0;
+      sd_adr <= 0;
   end
+  
+  // display_data[7:0] <= audio_data[7:0];  
+  // display_data[15:8] <= counter;
+  display_data[23:0] <= sd_adr[23:0];
+  // LED[7:0] <= fifo_count[7:0];
+  display_data[31:24] <= sd_state;
+
+  LED[15:8] <= sd_dout[7:0];   
+  LED[7:0] <= fifo_count[7:0]; 
+
+  // LED16_R <= fifo_full;
+  // LED16_B <= fifo_empty;
+  LED16_G <= sd_ready;
+  LED17_B <= sd_byte_available;
+  
+end
+
+
 
 
 // TESTING
